f5362288d22a34693c011976266a83e8ffb7660d
Srushti Shah <srushti1010shah@gmail.com>
2023-04-26 13:33:48 +0530

    Navbar and Bootstrap added, Tailwind removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <div className="text-2xl">
-      Hello, World!
+    <div className="App">
+      <Navbar/>
     </div>
   );
 }
