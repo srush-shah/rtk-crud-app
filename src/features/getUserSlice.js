42c3116f9fdc91939c73c5ca672b0105c2c050d6
Srushti Shah <srushti1010shah@gmail.com>
2023-04-26 16:25:02 +0530

    createUserAction async action created and reducer added to extrareducers in getUserSlice

--- src/features/getUserSlice.js
+++ src/features/getUserSlice.js
@@ -1,5 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+//Actions
+import { createUser } from "./actions/createUserAction";
+
 
 // User Slice
 export const gitUser = createSlice({
@@ -14,6 +17,17 @@ export const gitUser = createSlice({
         searchUser: (state,action) => {
             state.searchData = action.payload
         }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(createUser.pending, state => {
+            state.loading = true
+        }).addCase(createUser.fulfilled, (state, action) => {
+            state.loading = false,
+            state.users.push(action.payload)
+        }).addCase(createUser.rejected, (state,action) => {
+            state.loading = false,
+            state.error = action.payload
+        })
     }
 })
 
