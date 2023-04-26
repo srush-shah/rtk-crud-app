ec596519e9772db5844941f137841d6b045aaaee
Srushti Shah <srushti1010shah@gmail.com>
2023-04-26 19:09:58 +0530

    Data rendered in Read file using useSelector

--- src/components/Read.jsx
+++ src/components/Read.jsx
@@ -1,25 +1,38 @@
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getAllUsers } from '../features/actions/readUsersAction'
 
 const Read = () => {
     const dispatch = useDispatch()
+
     useEffect(() => {
         dispatch(getAllUsers())        
     },[dispatch])
+
+    const {users, loading} = useSelector(state => state.app)
+
+    if(loading) {
+        return <h2>Loading...</h2>
+    }
+
   return (
     <div>
         <h2>Users</h2>
         <div>
-            <div className="card w-50 mx-auto">
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    {/* <a href="#" className="card-link">Card link</a>
-                    <a href="#" className="card-link">Another link</a> */}
+            {users && users.map(user => (
+                <div key={user.id} className="card w-50 mx-auto my-2 d-flex">
+                    {/* <img src={user.avatar} alt="avatar" className='rounded-circle w-25 mx-auto my-2' /> */}
+                    <div className="card-body">
+                        <h4 className="card-title">{user.name}</h4>
+                        <h5 className="card-subtitle mb-2 text-body-secondary">{user.email}</h5>
+                        <p className="card-text">{user.age}</p>
+                        <a href="#" className="card-link">View</a>
+                        <a href="#" className="card-link">Edit</a>
+                        <a href="#" className="card-link">Delete</a>
+                    </div>
                 </div>
-            </div>
+            ))
+            }
         </div>
     </div>
   )
--- src/features/actions/readUsersAction.js
+++ src/features/actions/readUsersAction.js
@@ -7,11 +7,9 @@ export const getAllUsers = createAsyncThunk('getAllUsers', async (args, {rejectW
     const res = await fetch('https://6448ceb8b88a78a8f0f47996.mockapi.io/users')
 
     try {
-       const result = await res.json() 
-       console.log(result)
+       const result = await res.json()
        return result
     } catch (error) {
-        console.log(error)
        return rejectWithValue(error) 
     }
 })
